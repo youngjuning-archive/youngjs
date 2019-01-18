e1718624c945a06fe3b81c8e4a0fb70c8d6f487c
杨俊宁 <1003719811@qq.com>
2019-01-18 12:01:48 +0800

    dd

---
+++ dist/youngjs.cjs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// import YArray from './YArray'
+const YArray = require('./YArray');
+
+// export {
+//   YArray,
+// }
+//
+// export default {
+//   YArray,
+// }
+module.exports = {
+  YArray,
+};
+//# sourceMappingURL=youngjs.cjs.js.map
---
+++ dist/youngjs.es.js
@@ -0,0 +1,14 @@
+// import YArray from './YArray'
+var YArray = require('./YArray'); // export {
+//   YArray,
+// }
+//
+// export default {
+//   YArray,
+// }
+
+
+module.exports = {
+  YArray: YArray
+};
+//# sourceMappingURL=youngjs.es.js.map
