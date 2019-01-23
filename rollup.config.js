91bb4bc87930573551190ea00a091cd91ab3af71
杨俊宁 <1003719811@qq.com>
2019-01-23 12:03:02 +0800

    0.5.3

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 // rollup.config.js
 // https://juejin.im/post/5c073d86f265da615a419989
+// https://juejin.im/entry/57e159cd67f3560056bd2ba5
 // https://segmentfault.com/a/1190000010628352
 import babel from 'rollup-plugin-babel'
 
 export default {
   input: 'src/index.js',
   output: {
-    file: './dist/youngjs.es.js',
+    file: './dist/youngjs.js',
     format: 'es', // amd,cjs,es,iife,umd
-    sourcemap: true,
+    sourcemap: 'inline',
     exports: 'named',
   },
   plugins: [
