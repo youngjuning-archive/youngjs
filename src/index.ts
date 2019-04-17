9456b8eacfd328728508dc23f69e54d4289fe072
杨俊宁 <1003719811@qq.com>
2019-04-17 19:22:19 +0800

    定义返回值的类型

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { version } from '../package.json'
 
 // 数组并集
-function union(a: any[], b: any[]) {
+function union(a: any[], b: any[]): any[] {
   return Array.from(new Set(a.concat(b)))
 }
 
 // 根据指定key，对数组对象进行合并去重
-function unionByKey(a: object[], b: object[], key: string) {
+function unionByKey(a: object[], b: object[], key: string): object[] {
   const keys: any = {}
   const array = a.concat(b)
-  const result: any = []
+  const result: any[] = []
   array.forEach((item: any) => {
     if (!keys[item[key]]) {
       keys[item[key]] = item[key]
@@ -20,14 +20,14 @@ function unionByKey(a: object[], b: object[], key: string) {
 }
 
 // 数组交集
-function intersection(a: any[], b: any[]) {
+function intersection(a: any[], b: any[]): any[] {
   const aSet = new Set(a)
   const bSet = new Set(b)
   return Array.from(new Set(a.filter((v: any) => bSet.has(v))))
 }
 
 // 数组差集
-function difference(a: any[], b: any[]) {
+function difference(a: any[], b: any[]): any[] {
   const aSet = new Set(a)
   const bSet = new Set(b)
   return Array.from(new Set(a.concat(b).filter((v) => !aSet.has(v) || !bSet.has(v))))
